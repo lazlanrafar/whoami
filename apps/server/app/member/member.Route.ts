6b11dde6dec699949dccc03eab1c94f9a9ebf19a
lazlanrafar <lazlanrafar@gmail.com>
2023-12-08 19:57:08 +0700

    feat(server): update fetch api member

--- apps/server/app/member/member.Route.ts
+++ apps/server/app/member/member.Route.ts
@@ -3,8 +3,11 @@ import { GetMemberProject, GetMemberSkill } from "./member.Controller";
 import express from "express";
 const memberRouter = express.Router();
 
-memberRouter.get("/:userId", (req, res) => {
+memberRouter.get("/", (req, res) => {
   res.send("Welcome to Whoami API");
+});
+memberRouter.get("/:userId", (req, res) => {
+  res.send("Welcome to Whoami API " + req.params.userId);
 });
 memberRouter.get("/:userId/projects", GetMemberProject);
 memberRouter.get("/:userId/skills", GetMemberSkill);
